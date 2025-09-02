c10b46b386dc626b7389dd8820d9167406925980
zjqoyn <1401119288@qq.com>
2025-09-03 00:02:24 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+package class3.s2022350143202;
+
+import class3.s2022350143202.controller.LoginZJQController;
+
+public class Main {
+    static LoginZJQController login = new LoginZJQController();
+
+    public static void main(String[] args) {
+        login.login();
+    }
+}
